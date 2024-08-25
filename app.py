d0d879b43be3d95cf39184280cbedf42f46421bd
Luiz <valuiz1@gmail.com>
2024-08-25 11:37:27 -0300

    Changed route of himruser to a uuid

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from uuid import uuid4
+
+secret_page = uuid4()
 
 app = Flask(__name__)
 
@@ -6,8 +9,8 @@ with app.app_context():
 
     @app.route("/")
     def main():
-        return render_template("main.html")
+        return render_template("main.html", secret_page=secret_page)
 
-    @app.route("/hello")
+    @app.route(f"/{secret_page}")
     def hello():
         return render_template("hi_mr_user.html")
